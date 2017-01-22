9cefd94294aba442d2c8f9f19cb0447fd358bca4
Will Ockmore <will.ockmore@gmail.com>
2017-01-22 23:57:17 +0000

    remove unnecessary bits from index.js

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ const store = createStore(rootReducer);
 
 const rootEl = document.getElementById('app');
 
-let render = () => {
+const render = () => {
   const App = require('./App').default;
 
   ReactDOM.render(<App store={store} />, rootEl);
@@ -19,25 +19,8 @@ let render = () => {
 if (module.hot) {
   // Support hot reloading of components
   // and display an overlay for runtime errors
-  const renderApp = render;
-  const renderError = (error) => {
-    const RedBox = require('redbox-react');
-    ReactDOM.render(
-      <RedBox error={error} />,
-      rootEl
-    );
-  };
-
-  render = () => {
-    try {
-      renderApp();
-    } catch (error) {
-      renderError(error);
-    }
-  };
-
   module.hot.accept('./App', () => {
-    setTimeout(render);
+    render();
   });
 }
 
