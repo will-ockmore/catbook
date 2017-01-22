02c38bcd88bcabd1834541a3ad9228f4b97b0ab4
Will Ockmore <will.ockmore@gmail.com>
2017-01-22 23:58:01 +0000

    css fix for image selection, cat's ages

--- src/cats.js
+++ src/cats.js
@@ -5,6 +5,7 @@ const catData = fromJS([
     id: '0',
     meows: 0,
     name: 'Sebastian',
+    age: 5,
     about: 'Loves mice and long walks along the wall',
     avatar: 'http://www.cutestpaw.com/wp-content/uploads/2016/01/Watson-1.jpg'
   },
@@ -12,6 +13,7 @@ const catData = fromJS([
     id: '1',
     meows: 0,
     name: 'Simba',
+    age: 2,
     about: 'Lol any1 heading down the garden 2nite?',
     avatar: 'http://www.cutestpaw.com/wp-content/uploads/2016/02/Kitty-yoga.%281%29.jpg'
   },
@@ -19,6 +21,7 @@ const catData = fromJS([
     id: '2',
     meows: 0,
     name: 'Mr Tibbles',
+    age: 8,
     about: 'If a cat meows in an empty alley and no one is around to hear it, does it make a sound? #deep',
     avatar: 'http://www.cutestpaw.com/wp-content/uploads/2016/02/Nom-nom-nom....jpg'
   },
@@ -26,6 +29,7 @@ const catData = fromJS([
     id: '3',
     meows: 0,
     name: 'Max',
+    age: 3,
     about: 'Omg my humans are so lame',
     avatar: 'http://www.cutestpaw.com/wp-content/uploads/2016/02/Asked-for-a-cup-holder-got-a-cat-holder-instead.jpg'
   },
@@ -33,6 +37,7 @@ const catData = fromJS([
     id: '4',
     meows: 0,
     name: 'Fred',
+    age: 4,
     about: 'Plz meow me!11!! Will meow u back',
     avatar: 'http://www.cutestpaw.com/wp-content/uploads/2016/02/Snow-day.jpg'
   },
@@ -40,6 +45,7 @@ const catData = fromJS([
     id: '5',
     meows: 0,
     name: 'Brian',
+    age: 14,
     about: 'Think I\'m on the wrong site',
     avatar: 'http://www.cutestpaw.com/wp-content/uploads/2013/02/My-girlfriend-and-I-found-this-little-guy-on-the-beach-yesterday..jpg'
   },
